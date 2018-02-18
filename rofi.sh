7b89cb037b3123c7e39c981e7e786881b993e625
Frank <f.hoeben@gmail.com>
2018-02-18 16:16:38 +0100

    Initial upload

---
+++ rofi.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#rofi -show run -fullscreen -font "Fira Code 14" -fg "#B0E670" -bg "#14161F" -hlfg "#14161F" -hlbg "#B0E670" -bc "#14161F" -location 0 -lines 19 -bw 0 -font "MonacoB 16" -padding 350 -separator-style none -opacity 80 -hide-scrollbar 
+rofi -show run -font "Fira Code 14" -location 0 -lines 19 -bw 0 -font "Fira Code 16" -padding 350 -separator-style none -opacity 80 -hide-scrollbar 
